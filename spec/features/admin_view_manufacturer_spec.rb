968edc77cd7ed15034bb8f3b161994cf988467a6
renatasmendes <renatasmendes@hotmail.com>
2022-05-17 12:43:16 -0300

    Aula 02

---
+++ spec/features/admin_view_manufacturer_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+feature 'Admin view manufacturers' do
+  scenario 'successfully' do
+    # AAA
+    # Arrange
+    Manufacturer.create!(name: 'Fiat')
+    Manufacturer.create!(name: 'Volkswagen')
+
+    # Act
+    visit root_path
+    click_on 'Fabricantes'
+
+    # Asserts
+    expect(page).to have_content('Fiat')
+    expect(page).to have_content('Volkswagen')
+  end
+
+  scenario 'and view details' do
+    Manufacturer.create!(name: 'Fiat')
+    Manufacturer.create!(name: 'Volkswagen')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+
+    expect(page).to have_content('Fiat')
+    expect(page).to have_content('Volkswagen')
+  end
+
+  scenario 'and no manufacturers are created' do
+    visit root_path
+    click_on 'Fabricantes'
+
+    expect(page).to have_content('Nenhum fabricante cadastrado')
+  end
+
+  scenario 'and return to home page' do
+    Manufacturer.create!(name: 'Fiat')
+    Manufacturer.create!(name: 'Volkswagen')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'and return to manufacturers page' do
+    Manufacturer.create!(name: 'Fiat')
+    Manufacturer.create!(name: 'Volkswagen')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Voltar'
+
+    expect(current_path).to eq manufacturers_path
+  end
+end
